a27507f704b4eea63a2ece55f6189339f076c049
Elton <elton.b.braga@gmail.com>
2026-04-30 09:40:16 -0300

    ajuste

--- src/app/componentes/tela-principal/tela-principal.ts
+++ src/app/componentes/tela-principal/tela-principal.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+//import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tela-principal',
-  imports: [],
+  standalone: true,
+  imports: [MatIconModule],
   templateUrl: './tela-principal.html',
   styleUrl: './tela-principal.css',
 })
-export class TelaPrincipal {
-
-}
+export class TelaPrincipal {}
