104d0ca85fc8b1d09bf72e0a1bd4c5f5607d18f8
Elton <elton.b.braga@gmail.com>
2026-04-30 16:01:41 -0300

    .

--- src/app/componentes/tela-principal/tela-principal.ts
+++ src/app/componentes/tela-principal/tela-principal.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-//import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-tela-principal',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './tela-principal.html',
   styleUrl: './tela-principal.css',
 })
