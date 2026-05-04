f80da981e2adb21e81a7e54d2cdd28c8131b6b0b
Elton <elton.b.braga@gmail.com>
2026-05-04 13:03:43 -0300

    ajuste

--- src/app/componentes/tela-principal/assinar-docs/assinar-docs.ts
+++ src/app/componentes/tela-principal/assinar-docs/assinar-docs.ts
@@ -1,27 +1,22 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-assinar-docs',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatStepperModule,
-    MatButtonModule,
-    MatIconModule,
-    NgxExtendedPdfViewerModule,
-    NgIf,
-  ],
+  imports: [CommonModule, MatStepperModule, MatButtonModule, MatIconModule],
   templateUrl: './assinar-docs.html',
   styleUrl: './assinar-docs.css',
 })
 export class AssinarDocs {
   file: File | null = null;
-  pdfSrc: string | null = null;
+  pdfSrc: SafeResourceUrl | null = null;
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   removeFile() {
     this.file = null;
@@ -54,7 +49,8 @@ export class AssinarDocs {
 
   private loadPdf(file: File) {
     if (file.type === 'application/pdf') {
-      this.pdfSrc = URL.createObjectURL(file);
+      const url = URL.createObjectURL(file);
+      this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(url);
     } else {
       this.pdfSrc = null;
     }
