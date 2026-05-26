2f33337f89b332811f75465c218c85ba8e3ab0b8
Elton <elton.b.braga@gmail.com>
2026-05-25 22:03:54 -0300

    ajuste

--- src/app/componentes/resolucao-pendencias/container/solicitacao/solicitacao.ts
+++ src/app/componentes/resolucao-pendencias/container/solicitacao/solicitacao.ts
@@ -1,20 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import { RELATORIO_INCRA_MOCK } from '../../dados/relatorio-incra.mock';
 
 @Component({
   selector: 'app-solicitacao',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './solicitacao.html',
   styleUrl: './solicitacao.css',
 })
 export class Solicitacao {
   relatorio = RELATORIO_INCRA_MOCK;
 
-  getValidacao(categoria: string) {
-    return this.relatorio.validacoes.find(
-      (item) => item.categoria === categoria,
-    );
+  pendencias = this.relatorio.validacoes.flatMap((categoria) =>
+    categoria.itens
+      .filter((item) => item.status.toLowerCase() === 'pendente')
+      .map((item) => ({
+        categoria: categoria.categoria,
+        titulo: item.titulo,
+        status: item.status,
+
+        descricao: '',
+        especificacao: '',
+        justificativa: '',
+      })),
+  );
+
+  cancelarPendencia(pendencia: any): void {
+    pendencia.especificacao = '';
+    pendencia.justificativa = '';
+  }
+
+  salvarResolucao(pendencia: any): void {
+    console.log('Pendência salva:', pendencia);
   }
 
   getStatusClass(status: string): string {
