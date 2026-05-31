eb7cac3ad7b4fc534de6a449c463772315b127d4
Elton <elton.b.braga@gmail.com>
2026-05-31 04:13:59 -0300

    ajuste

--- src/app/acompanhamento-de-solicitacoes/container/tela-acompanhamento/tela-acompanhamento.ts
+++ src/app/acompanhamento-de-solicitacoes/container/tela-acompanhamento/tela-acompanhamento.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importação necessária para as diretivas estruturais
+import { CommonModule } from '@angular/common';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { SolicitacaoAcompanhamento } from '../../dados/acompanhamento.interface';
 import { ACOMPANHAMENTO_SOLICITACOES_MOCK } from '../../dados/acompanhamento.mock';
 
 @Component({
   selector: 'app-tela-acompanhamento',
-  standalone: true, // Garante que o componente aceita imports diretamente
-  imports: [CommonModule, MatButtonModule, MatIconModule], // Adicionado CommonModule aqui
+  standalone: true,
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './tela-acompanhamento.html',
   styleUrl: './tela-acompanhamento.css',
 })
